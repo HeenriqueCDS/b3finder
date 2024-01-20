43528e6c2cb0f0c9b1086aafa427fdfb1dc16f75
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-20 18:13:31 -0300

    feat: codegen

---
+++ contract/codegen.ts
@@ -0,0 +1,27 @@
+import type { CodegenConfig } from "@graphql-codegen/cli"
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: "./schema.graphql",
+//   documents: "../app/**/!(*.d).{ts,tsx}",
+  generates: {
+    "../server/gql/schema.ts": {
+      plugins: [
+        {
+          typescript: {
+            enumsAsConst: true,
+          },
+        },
+        "typescript-resolvers",
+      ],
+    },
+    "./client/": {
+      preset: "client",
+      config: {
+        enumsAsTypes: true,
+      },
+    },
+  },
+}
+
+export default config
