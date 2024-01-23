26aedb24e74d37b764d1391e90b655b923e2f110
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-23 15:42:41 -0300

    feat: import type defs

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,53 +4,15 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { Resolvers } from "../gql/schema";
 import { db } from "./database/client";
 import { importTicker } from "./queue/sqs";
-
-const typeDefs = `#graphql
-  scalar Date
-  type History {
-    quoteSymbol: String!
-    date: Int!
-    open: Float!
-    high: Float!
-    low: Float!
-    volume: Int!
-    adjustedClose: Float!
-  }
-  type Query {
-    history(quoteSymbol: String!): [History]
-  }
-
-  type Quote {
-    symbol: String!
-    currency: String!
-    shortName: String!
-    longName: String!
-    regularMarketPrice: Float!
-    regularMarketChange: Float!
-    regularMarketChangePercent: Float!
-    logoUrl: String!
-    updatedAt: Date!
-    fiftyTwoWeekLow: Float!
-    fiftyTwoWeekHigh: Float!
-    marketCap: Float!
-    regularMarketVolume: Float!
-    regularMarketOpen: Float!
-    regularMarketDayHigh: Float!
-    regularMarketDayLow: Float!
-    regularMarketPreviousClose: Float!
-  }
-  type Query {
-    quotes: [Quote!]!
-  }
-  type Query {
-    quote(symbol: String!): Quote
-  }
-`;
+import { typeDefs } from "./type-defs";
 
 const resolvers: Resolvers = {
   Query: {
     history: async (parent, args, context) => {
-      const history = await db.history.findMany({where: {quoteSymbol: args.quoteSymbol}, orderBy: {date: "asc"}});
+      const history = await db.history.findMany({
+        where: { quoteSymbol: args.quoteSymbol },
+        orderBy: { date: "asc" },
+      });
       return history;
     },
     quotes: async (parent, args, context) => {
@@ -58,16 +20,17 @@ const resolvers: Resolvers = {
       return quotes;
     },
     quote: async (parent, args, context) => {
-      const quote = await db.quote.findUnique({where: {symbol: args.symbol}});
+      const quote = await db.quote.findUnique({
+        where: { symbol: args.symbol },
+      });
 
       if (!quote) {
-        await importTicker(args.symbol)
-        return null
+        await importTicker(args.symbol);
+        return null;
       }
 
       return quote;
     },
-
   },
 };
 
