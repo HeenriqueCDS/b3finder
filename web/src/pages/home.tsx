e5be679e282b972166b578861959b982d3c641ed
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-24 10:23:58 -0300

    web: create header

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { MagnifyingGlass } from "@phosphor-icons/react"
 import { useQuery } from "@tanstack/react-query"
 import request, { gql } from "graphql-request"
 
@@ -26,11 +27,18 @@ export const Home = () => {
             return quotes
         }
     })
-    console.log(data)
+
     isLoading && <div>Loading...</div>
     return (
-        <ul>
+        <div className="h-screen w-screen text-neutral-300 bg-neutral-950">
+            <header className="w-screen p-4 border-b-neutral-900 border-b-2 flex flex-col gap-4">
+                <h1 className="text-3xl text-neutral-400"><span className="text-green-400">[B]</span><span className="text-yellow-400">³</span> Finder</h1>
+                <div className="flex rounded-md border-neutral-900 border-2 justify-between">
+                    <input className="bg-transparent text-neutral-300 p-2 focus:outline-none" type="text" placeholder="Search..." />
+                    <button className="bg-transparent text-neutral-300 p-2 border-l-2 border-l-neutral-900 min-w-10 flex justify-center items-center"><MagnifyingGlass className="text-neutral-700  w-5 h-5"/></button>
+                </div>
+            </header>
             {data?.map((quote) => { return <li><a href={`/${quote.symbol}`}>{quote.symbol}</a></li> })}
-        </ul>
+        </div>
     )
 }
