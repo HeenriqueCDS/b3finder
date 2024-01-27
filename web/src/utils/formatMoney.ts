2178cad977bd8e5a343311650f8fe54bff0e953a
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-27 13:30:52 -0300

    web: format billion and million

--- web/src/utils/formatMoney.ts
+++ web/src/utils/formatMoney.ts
@@ -1,8 +1,34 @@
-export const formatMoney = (value: number) => {
-    return new Intl.NumberFormat('pt-BR', {
-        style: 'currency',
-        maximumFractionDigits: 2,       
-        compactDisplay: 'short',
-        currency: 'BRL',
-    }).format(value);
-}
+const isBillion = (value: number): boolean => {
+  return value >= 1e9;
+};
+
+const formatBillion = (value: number): string => {
+  return (
+    "R$ " +
+    (value / 1e9).toLocaleString(undefined, { maximumFractionDigits: 1 }) +
+    " bi"
+  );
+};
+
+const isMillion = (value: number) => {
+  return value >= 1e6;
+};
+
+const formatMillion = (value: number): string => {
+  return (
+    "R$ " +
+    (value / 1e6).toLocaleString(undefined, { maximumFractionDigits: 1 }) +
+    " mi"
+  );
+};
+
+export const formatMoney = (value: number): string => {
+  if (isBillion(value)) return formatBillion(value);
+  if (isMillion(value)) return formatMillion(value);
+  return new Intl.NumberFormat("pt-BR", {
+    style: "currency",
+    maximumFractionDigits: 2,
+    compactDisplay: "short",
+    currency: "BRL",
+  }).format(value);
+};
