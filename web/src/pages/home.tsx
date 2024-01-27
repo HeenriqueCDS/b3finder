2d3e882e813e4f7166b5e4b74ed9d8043afbc8e5
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-27 13:32:43 -0300

    web: refactor

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import request, { gql } from "graphql-request"
-import { useNavigate } from "react-router-dom"
+import { QuoteCard } from "../components/quote-card"
 interface QuotesQuery {
     quotes: {
         symbol: string
@@ -12,7 +12,6 @@ interface QuotesQuery {
 }
 
 export const Home = () => {
-    const navigate = useNavigate()
     const listQuotesQueryDocument = gql`
     query quotes {
       quotes {
@@ -37,32 +36,8 @@ export const Home = () => {
         <div className="flex flex-col p-4 gap-4 h-[calc(100%-130px)] sm:h-[calc(100%-78px)]">
             <h1 className="text-neutral-400 text-2xl">Quotes</h1>
             <div className="flex flex-col gap-3 overflow-auto max-h-full">
-                {data?.sort((a, b) => {
-                    if (a.regularMarketPrice > b.regularMarketPrice) {
-                        return -1
-                    }
-                    if (a.regularMarketPrice < b.regularMarketPrice) {
-                        return 1
-                    }
-                    return 0
-                }).map((quote) => {
-                    return (
-                        <article onClick={() => navigate(`/${quote.symbol}`)} className=" max-w-full inline-flex justify-between p-3 items-center border-2 rounded-md transition-all border-neutral-900 hover:-translate-y-1 cursor-pointer">
-                            <div className="flex gap-3 w-[calc(100%-80px)] overflow-hidden truncate">
-                                <img src={quote.logoUrl} className="h-10 w-10 rounded-md" />
-                                <div className="flex flex-col gap-1 w-[calc(100%-45px)]">
-                                    <h2 className="text-base text-neutral-300 truncate">{quote.symbol}</h2>
-                                    <h2 className="text-base text-neutral-500 truncate">{quote.longName}</h2>
-                                </div>
-                            </div>
-                            <h3 className="text-green-300 text-base w-20 text-end">
-                                {quote.regularMarketPrice}{' '}
-                                <span className="text-neutral-300 text-xs">
-                                    {quote.currency}
-                                </span>
-                            </h3>
-                        </article>
-                    )
+                {data?.map((quote) => {
+                    return <QuoteCard key={quote.symbol} {...quote} />
                 })}
             </div>
 
