ded69571544f28144d0b47fc3be7473c22762c08
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-29 14:23:29 -0300

    server: remove search from header

--- web/src/components/header/index.tsx
+++ web/src/components/header/index.tsx
@@ -1,8 +1,7 @@
 import { useNavigate } from 'react-router-dom'
-import { Search } from '../search'
 
 
-export const Header = ({}) => {
+export const Header = () => {
     const navigate = useNavigate()
     const handleLogoClick = () => {
         navigate('/')
@@ -10,7 +9,7 @@ export const Header = ({}) => {
     return (
         <header className="w-screen p-4 border-b-neutral-900 border-b-2 flex flex-col gap-4 sm:flex-row sm:justify-between cursor-pointer">
             <h1 className="text-3xl text-neutral-400" onClick={() => handleLogoClick()}><span className="text-green-400">[B]</span><span className="text-yellow-400">³</span> Finder</h1>
-            <Search />
+
         </header>
     )
 }
