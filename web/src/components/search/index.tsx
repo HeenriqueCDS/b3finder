3dd41be25621a420d1949f04b784f3bf2709be63
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-29 14:24:57 -0300

    web: remove state from search component

--- web/src/components/search/index.tsx
+++ web/src/components/search/index.tsx
@@ -1,21 +1,18 @@
-import { useState } from "react"
-import { useQuotes } from "../../hooks/use-quotes"
 import { SearchButton } from "./button"
 import { SearchInput } from "./input"
-import { SearchList } from "./search-list"
 
-export const Search = () => {
-    const [search, setSearch] = useState<string>('')
-    const showSearchList = search.length > 1
-    const { data } = useQuotes()
+interface SearchProps {  
+    search: string
+    setSearch: (value: string) => void
+
+}
+
+export const Search = ({search, setSearch}: SearchProps) => {
 
     return (
         <div className="flex rounded-md border-neutral-900 border-2 justify-between max-w-80 relative">
             <SearchInput search={search} setSearch={setSearch} />
             <SearchButton />
-            {showSearchList &&
-                <SearchList data={data!} search={search} />
-            }
         </div>
     )
 }
