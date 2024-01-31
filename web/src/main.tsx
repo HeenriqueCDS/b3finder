699284ed503541e2e43b319de2a6c48ae0714086
HeenriqueCDS <heenriquecds@gmail.com>
2024-01-31 10:42:09 -0300

    web: add ToastContainer

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -2,6 +2,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import App from './App.tsx'
 import './styles/index.css'
 
@@ -10,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
     <ReactQueryDevtools initialIsOpen={false} />
+    <ToastContainer />
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
